9ab2dcad5a0361f211ebc63764a3e993f5b7b009
anna_vezhdel <avezhdel@gmail.com>
2026-03-06 12:29:10 +0100

    icon

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,19 +17,13 @@ export const metadata: Metadata = {
   icons: {
     icon: [
       {
-        url: "/icon-light-32x32.png",
-        media: "(prefers-color-scheme: light)",
-      },
-      {
-        url: "/icon-dark-32x32.png",
-        media: "(prefers-color-scheme: dark)",
+        url: "/icon.svg",
       },
       {
         url: "/icon.svg",
         type: "image/svg+xml",
       },
     ],
-    apple: "/apple-icon.png",
   },
 };
 
